22cee59197042994da77c1bcf7693541ec611485
DeMarcus Kirby <tkd024@gmail.com>
2020-06-02 18:04:56 -0600

    re json items
    Co-Authored-By: Paul Debevec <PaulDebevec@users.noreply.github.com>

--- sinatra_service.rb
+++ sinatra_service.rb
@@ -5,12 +5,14 @@ require 'pry'
 
   get '/quote' do
     response = Faraday.get("https://type.fit/api/quotes")
-    JSON.parse(response.body, symbolize_names: true).sample
+    quote_obj = JSON.parse(response.body, symbolize_names: true).sample
+    quote_obj.to_json
   end
 
   get '/playlists/:token' do
     response = Faraday.get("https://api.spotify.com/v1/me/playlists") do |f|
       f.headers["Authorization"] = "Bearer #{params[:token]}"
     end
-    JSON.parse(response.body, symbolize_names: true)
+    playlists = JSON.parse(response.body, symbolize_names: true)
+    playlists.to_json
   end
