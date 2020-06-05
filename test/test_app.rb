746a0026879cf04fb4743b195e0009dbd817ab8d
Paul Debevec <paulmdebevec@gmail.com>
2020-06-04 21:41:10 -0600

    Add test for spotify endpoint

--- test/test_app.rb
+++ test/test_app.rb
@@ -12,7 +12,21 @@ class MyTest < MiniTest::Test
     response = get '/quote'
     formatted = JSON.parse(response.body, symbolize_names: true)
 
+    assert_instance_of(Hash, formatted)
     assert formatted.include?(:text)
     assert formatted.include?(:author)
+  end
+
+  def test_it_returns_a_list_of_playlists
+    skip
+    # ENV Variable broken - Test passes with hard coded token
+    response = get "/playlists/#{ENV['SPOTIFY_TOKEN']}"
+    formatted = JSON.parse(response.body, symbolize_names: true)
+
+    assert_instance_of(Hash, formatted)
+    assert formatted[:items][0].include?(:href)
+    assert formatted[:items][0].include?(:name)
+    assert formatted[:items][2].include?(:href)
+    assert formatted[:items][2].include?(:name)
   end
 end
