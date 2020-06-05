7a884a2f70fb0ee1efab9c810948a3a61fb30fdf
Paul Debevec <paulmdebevec@gmail.com>
2020-06-04 21:05:38 -0600

    Add test for quote endpoint

--- test/test_app.rb
+++ test/test_app.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
+require 'sinatra_service.rb'
 
 class MyTest < MiniTest::Test
-  def test_my_app
-    # insert test here
+  include Rack::Test::Methods
+
+  def app
+    JamSessionService
+  end
+
+  def test_it_returns_a_quote
+    response = get '/quote'
+    formatted = JSON.parse(response.body, symbolize_names: true)
+
+    assert formatted.include?(:text)
+    assert formatted.include?(:author)
   end
 end
